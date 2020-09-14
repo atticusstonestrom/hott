dd862a20419129c8489888ceec793e7ab7eee763
atticusstonestrom <atticusstonestrom@yahoo.com>
2020-09-14 16:22:21 -0400

    Update main.c

--- main.c
+++ main.c
@@ -5,4 +5,9 @@ chat input[2048];
 //catch ctrl c interrupt
 
 int main(int argc, char **argv) {
-	puts("press "); }
+	puts("press ctrl+d to exit");
+	while(1) {
+		fputs("idris> ", stdout);
+		fgets(input, 2048, stdin);
+		puts("\t%s", input); }
+	return 0; }
