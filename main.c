efab4ee59ffbb3362b1c3d530efa7ead888744a2
atticusstonestrom <atticusstonestrom@yahoo.com>
2020-09-15 23:59:36 -0400

    Update main.c

--- main.c
+++ main.c
@@ -11,22 +11,45 @@ typedef struct {
 	char *type;
 } atom_t;
 
+typedef type_t char *;
+typedef var_t  char *;
+
 typedef struct ctx_atom_t {
 	//struct ctx_atom_t *next;
-	char *var;
-	char *type;
+	var_t var;
+	type_t type;
 } ctx_atom_t;
+
+//should context be a hash map? much more efficient
+//memoization
+//first check built-in types, then check axioms
+
+//to check if ascii, use mask:	?
+//	0x000000
+
+int judge_same_type() {
+	hypothesissame;
+	typesame;
+	return 0; }
 
 int judge_context(ctx_atom_t *gamma) {
 	if(gamma->var==NULL) {
 		return 1; }
 	if(!judge_same_type(gamma->type, gamma->type)) {
 		return 0; }
-	int i;
+	//int i;
 	//for(i=0; (gamma+i)->var!=NULL; i++) {	//not really efficient, O(n!)
 	//	if(str_eq(gamma->var, (gamma+i)->var)) { return 0; }}
 	//return judge_context(gamma->next);
 	return judge_context(gamma+1); }
+
+int judge_lookup(ctx_atom_t *gamma, var_t *to_lookup, type_t *to_fill) {
+	if(gamma->var==NULL) {
+		return 0; }
+	if(str_eq(gamma->var, to_lookup)) {
+		strncpy(to_fill, gamma->type);
+		return 1; }
+	return judge_lookup(gamma+1, to_lookup, to_fill); }
 
 //sort all judgements by type of hypotheses for efficient lookup
 
